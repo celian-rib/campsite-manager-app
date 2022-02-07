4356de74d3113d55b80c6b6c94010f532c335070
celian <celian.riboulet@gmail.com>
2022-02-07 13:48:31 +0100

    Rename main class

--- src/main/java/pt4/flotsblancs/App.java
+++ src/main/java/pt4/flotsblancs/App.java
@@ -8,7 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
-public class HelloWorld extends Application {
+public class App extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
