a2c71ba107ffc362065737713a86c9001487c222
gallon emilien <gallonemilien@gmail.com>
2022-02-14 19:23:44 +0100

    edit pom xml for build

---
+++ launchApp.bat
@@ -0,0 +1,2 @@
+java --module-path "C:\Users\Emilien\Documents\03-Dev\PT4\javafx-sdk-17.0.2\lib" --add-modules javafx.controls,javafx.fxml -jar target/flots-blancs-1.0.0-jar-with-dependencies.jar
+pause
