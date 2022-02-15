b5eeb22cd9d06ab8152e9cb834a7c873e5d65a86
emgallon <gallonemilien@gmail.com>
2022-02-15 11:03:42 +0100

    doc du build

--- launchApp.sh
+++ launchApp.sh
@@ -1,2 +1,1 @@
-java --module-path "PATH TO JAVA FX" --add-modules javafx.controls,javafx.fxml -jar target/flots-blancs-1.0.0-jar-with-dependencies.jar
-pause
+java --module-path "../JFX/lib" --add-modules javafx.controls,javafx.fxml -jar target/flots-blancs-1.0.0-jar-with-dependencies.jar
