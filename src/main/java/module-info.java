2a656ce979effb9466638db9b9a40c6facfb4621
Riboulet Celian <celian.riboulet@etu.u-bordeaux.fr>
2022-02-16 11:33:49 +0100

    Ajout fontawesome icon pack

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,14 +6,18 @@ module pt4.flotsblancs {
 	requires ormlite.jdbc;
 	requires java.sql;
 	requires dotenv.java;
-	
+
 	requires MaterialFX;
 	requires lombok;
-	
-	opens pt4.flotsblancs to javafx.graphics,ormlite.jdbc;
-	opens pt4.flotsblancs.database to javafx.graphics,ormlite.jdbc;
+
+	requires org.kordamp.ikonli.core;
+	requires org.kordamp.ikonli.fontawesome5;
+	requires org.kordamp.ikonli.javafx;
+
+	opens pt4.flotsblancs to javafx.graphics, ormlite.jdbc;
+	opens pt4.flotsblancs.database to javafx.graphics, ormlite.jdbc;
 	opens pt4.flotsblancs.database.model to ormlite.jdbc;
-	
+
 	exports pt4.flotsblancs;
 	exports pt4.flotsblancs.database;
 	exports pt4.flotsblancs.database.model;
