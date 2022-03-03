ca2a6f4abc24029b0bf4d611e1a70c66f2cb96c4
Moreno <ruben.moreno@bineex.com>
2022-03-03 10:07:16 +0100

    tableView fonctionnelle

--- src/main/java/pt4/flotsblancs/scenes/StocksScene.java
+++ src/main/java/pt4/flotsblancs/scenes/StocksScene.java
@@ -1,6 +1,8 @@
 package pt4.flotsblancs.scenes;
 
-import com.github.javafaker.Stock;
+import java.sql.SQLException;
+import java.util.List;
+
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -8,11 +10,14 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import pt4.flotsblancs.database.Database;
+import pt4.flotsblancs.database.model.Stock;
 import pt4.flotsblancs.router.IScene;
 
-public class StocksScene extends VBox {
+public class StocksScene extends VBox implements IScene {
 
     public String getName() {
         return "Stocks";
@@ -23,28 +28,13 @@ public class StocksScene extends VBox {
     }
 
     public void onFocus() {
-        TableView<Stock> table = new TableView<Stock>();
-
-      // Create column UserName (Data type of String).
-      TableColumn<Stock, String> itemCol //
-              = new TableColumn<Stock, String>("Objet");
-
-      // Create column Email (Data type of String).
-      TableColumn<Stock, Integer> quantityCol//
-              = new TableColumn<Stock, Integer>("Quantité");
-
-      // Create column FullName (Data type of String).
-      TableColumn<Stock, String> storageLocCol//
-              = new TableColumn<Stock, String>("Emplacement");
-
-      // Active Column
-      TableColumn<Stock, Boolean> activeCol//
-              = new TableColumn<Stock, Boolean>("Active");
-
-      table.getColumns().addAll(itemCol, quantityCol, storageLocCol, activeCol);
-
-      this.getChildren().add(table);
+        
     }
+
+    protected List<Stock> queryAll() throws SQLException {
+        return Database.getInstance().getStockDao().queryForAll();
+    }
+
 
     public void onUnfocus() {
 
@@ -52,12 +42,32 @@ public class StocksScene extends VBox {
 
     public void start() {
         setAlignment(Pos.CENTER);
+        displayTableView();
+    }
 
-        // Création des élèments de cette page
-        Label label = new Label(this.getName());
+    public void displayTableView(){
+        TableView<Stock> table = new TableView<Stock>();
+        
+        table.setEditable(true);
 
-        // On ajoute tous les élèments de cette page comme enfant de BaseScene
-        // Ils seront grace à cela affichés.
-        getChildren().addAll(label);
+        TableColumn<Stock, String> itemCol = new TableColumn<Stock, String>("Objet");
+
+        TableColumn<Stock, Integer> quantityCol = new TableColumn<Stock, Integer>("Quantité");
+
+        TableColumn<Stock, String> storageLocCol = new TableColumn<Stock, String>("Emplacement");
+
+        table.getColumns().addAll(itemCol, quantityCol, storageLocCol);
+
+        itemCol.setCellValueFactory(new PropertyValueFactory<>("item"));
+        quantityCol.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+        storageLocCol.setCellValueFactory(new PropertyValueFactory<>("storageLocation"));
+
+        try {
+            table.getItems().addAll(queryAll());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        this.getChildren().add(table);
     }
 }
