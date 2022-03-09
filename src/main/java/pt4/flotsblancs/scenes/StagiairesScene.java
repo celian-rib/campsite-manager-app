d4cb1e71f308e8454608049a58b15021ceb23a67
Hugo Martin <hugo.martin.duloz@gmail.com>
2022-03-09 18:05:22 +0100

    fini pour ajd

--- src/main/java/pt4/flotsblancs/scenes/StagiairesScene.java
+++ src/main/java/pt4/flotsblancs/scenes/StagiairesScene.java
@@ -1,19 +1,21 @@
 package pt4.flotsblancs.scenes;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 
 import java.sql.SQLException;
 import java.util.List;
 
-import javafx.geometry.Pos;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import pt4.flotsblancs.database.Database;
 import pt4.flotsblancs.database.model.User;
-import pt4.flotsblancs.router.IScene;
 import pt4.flotsblancs.scenes.items.ItemScene;
 
-public class StagiairesScene extends ItemScene<User> implements IScene {
+public class StagiairesScene extends ItemScene<User> {
 
     @Override
     public String getName() {
@@ -38,8 +40,18 @@ public class StagiairesScene extends ItemScene<User> implements IScene {
     @Override
     protected Region createContainer(User item) {
         VBox container = new VBox();
-        Label label = new Label(item.getDisplayName());
-        container.getChildren().add(label);
+        container.setAlignment(Pos.CENTER);
+        container.setPadding(new Insets(5));
+        
+        MFXButton resetBtn = new MFXButton("Reset password.");
+        MFXTextField usernameTxtFld = new MFXTextField(item.getLogin());
+        MFXTextField firstNameTxtFld = new MFXTextField(item.getFirstName());
+        MFXTextField lastNameTxtFld = new MFXTextField(item.getName());
+        Label roleLabel = new Label("Rôle : " + (item.isAdmin() ? "Administrateur" : "Stagiaire"));
+        
+        
+        Label titleLabel = new Label("#" + item.getId() + " - " + item.getDisplayName());
+        container.getChildren().addAll(titleLabel, roleLabel, firstNameTxtFld, lastNameTxtFld, usernameTxtFld, resetBtn);
         return container;
     }
 
