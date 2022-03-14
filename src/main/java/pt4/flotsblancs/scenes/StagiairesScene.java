0099988f0441d9edf7e7b8448024249e0778579c
Hugo Martin <hugo.martin.duloz@gmail.com>
2022-03-14 10:15:00 +0100

    Page stagiares fonctionnelle 100%, UI à améliorer

--- src/main/java/pt4/flotsblancs/scenes/StagiairesScene.java
+++ src/main/java/pt4/flotsblancs/scenes/StagiairesScene.java
@@ -7,11 +7,13 @@ import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
 
 import java.sql.SQLException;
+import java.sql.SQLRecoverableException;
 import java.util.List;
 
 
 
 import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXCheckbox;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -21,7 +23,10 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import pt4.flotsblancs.database.Database;
 import pt4.flotsblancs.database.model.User;
+import pt4.flotsblancs.router.Router;
+import pt4.flotsblancs.router.Router.Routes;
 import pt4.flotsblancs.scenes.items.ItemScene;
+import pt4.flotsblancs.scenes.utils.ToastType;
 
 public class StagiairesScene extends ItemScene<User> {
 
@@ -30,6 +35,9 @@ public class StagiairesScene extends ItemScene<User> {
     private User stagiaire;
     private MFXTextField firstName;
     private MFXTextField lastName;
+    private MFXTextField usernameTxtFld;
+    private Spinner<Integer> heures;
+    private MFXCheckbox isAdmin;
 
     @Override
     public String getName() {
@@ -44,11 +52,6 @@ public class StagiairesScene extends ItemScene<User> {
     @Override
     public void onUnfocus() {
         onContainerUnfocus();
-    }
-
-    @Override
-    public void onContainerUnfocus() {
-        //refreshDatabase();
     }
 
     /**
@@ -96,7 +99,7 @@ public class StagiairesScene extends ItemScene<User> {
         
         
         var credentialsLabel = new Label("Identifiants");
-        MFXTextField usernameTxtFld = new MFXTextField(stagiaire.getLogin());
+        usernameTxtFld = new MFXTextField(stagiaire.getLogin());
         MFXButton resetBtn = new MFXButton("Reset password.");
         resetBtn.getStyleClass().add("action-button");
         VBox credentials = new VBox(4, credentialsLabel, usernameTxtFld, resetBtn);
@@ -116,12 +119,21 @@ public class StagiairesScene extends ItemScene<User> {
         
         var heuresLabel = new Label("Heures hebdomadaires");
         SpinnerValueFactory<Integer> vFact = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 7*24, stagiaire.getWeeklyHours());
-        Spinner<Integer> heures = new Spinner<Integer>();
+        heures = new Spinner<Integer>();
         heures.setEditable(true);
         heures.setValueFactory(vFact);
 
+        var adminLabel = new Label("Admin ?");
+
+        isAdmin = new MFXCheckbox();
+        isAdmin.setSelected(stagiaire.isAdmin());
         
 
+
+        VBox middle = new VBox(adminLabel, isAdmin);
+
+        
+        container.setCenter(middle);
         container.setLeft(heuresLabel);
         container.setRight(heures);
         return container;
@@ -141,6 +153,48 @@ public class StagiairesScene extends ItemScene<User> {
         
         container.getChildren().addAll(createHeader(), createTopSlot(), createMiddleSlot());
         return container;
+    }
+
+    private void updateDatabase(User stagiaire) {
+        if (stagiaire == null)
+            return;
+        try {
+            if (!stagiaire.getFirstName().equals( firstName.getText().trim() )) {
+
+                stagiaire.setFirstName(firstName.getText().trim());
+            }
+            if (!stagiaire.getName().equals( lastName.getText().trim() )) {
+
+                stagiaire.setName(lastName.getText().trim());
+            }
+            if (!stagiaire.getLogin().equals( usernameTxtFld.getText().trim() )) {
+
+                stagiaire.setLogin(usernameTxtFld.getText().trim());
+            }
+            if (!stagiaire.getWeeklyHours().equals( heures.getValue() )) {
+
+                stagiaire.setWeeklyHours(heures.getValue());
+            }
+            if (!stagiaire.isAdmin() == isAdmin.selectedProperty().get() ) {
+
+                stagiaire.setAdmin(isAdmin.selectedProperty().get());
+            }
+            Database.getInstance().getUsersDao().update(stagiaire);
+            Router.showToast(ToastType.SUCCESS, "Stagiaire mis à jour");
+        } catch (SQLRecoverableException e) {
+            e.printStackTrace();
+            Router.showToast(ToastType.ERROR, "Erreur de connexion");
+            Router.goToScreen(Routes.CONN_FALLBACK);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            Router.showToast(ToastType.ERROR, "Erreur de mise à jour...");
+            Router.goToScreen(Routes.HOME);
+        }
+    }
+    
+    @Override
+    public void onContainerUnfocus() {
+            updateDatabase(stagiaire);
     }
 
     @Override
