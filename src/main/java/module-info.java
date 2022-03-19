b249eb0ba063c0d56a9f83ddf507e68e331ea35d
celian <celian.riboulet@gmail.com>
2022-03-19 14:53:05 +0100

    marche juste pas

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,24 +1,24 @@
 module pt4.flotsblancs {
-	requires javafx.base;
-	requires javafx.controls;
-	requires transitive javafx.graphics;
+	requires transitive static javafx.base;
+	requires transitive static javafx.controls;
+	requires transitive static javafx.graphics;
 
-	requires ormlite.jdbc;
-	requires java.sql;
-	requires dotenv.java;
+	requires transitive static ormlite.jdbc;
+	requires transitive static java.sql;
+	requires transitive static dotenv.java;
 
-	requires MaterialFX;
-	requires lombok;
-	requires javafaker;
-	requires virtualizedfx;
-	requires itextpdf;
+	requires transitive static MaterialFX;
+	requires transitive static static lombok;
+	requires transitive static javafaker;
+	requires transitive static virtualizedfx;
+	requires transitive static itextpdf;
 	
 	opens pt4.flotsblancs to javafx.graphics,ormlite.jdbc;
 	opens pt4.flotsblancs.database to javafx.graphics,ormlite.jdbc;
 
-	requires org.kordamp.ikonli.core;
-	requires org.kordamp.ikonli.fontawesome5;
-	requires org.kordamp.ikonli.javafx;
+	requires transitive static org.kordamp.ikonli.core;
+	requires transitive static org.kordamp.ikonli.fontawesome5;
+	requires transitive static org.kordamp.ikonli.javafx;
 
 	opens pt4.flotsblancs.database.model to ormlite.jdbc;
 
