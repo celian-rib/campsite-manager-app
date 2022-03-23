259d0f078bd5f255e9ccf14aac9d1bd24b817e73
celian <celian.riboulet@gmail.com>
2022-03-23 22:52:54 +0100

    Artifact ++

--- scripts/buildArtifact.sh
+++ scripts/buildArtifact.sh
@@ -6,15 +6,15 @@ LAUNCH_CMD="java --module-path"
 MODULE_ARGS="--add-modules javafx.controls,javafx.fxml -jar"
 
 echo "Starting project build script"
+mkdir flotblancs
 
 echo "Downloading javafx"
-curl https://download2.gluonhq.com/openjfx/18/openjfx-18_linux-x64_bin-sdk.zip > javafx-sdk.zip
+curl https://download2.gluonhq.com/openjfx/18/openjfx-18_linux-x64_bin-sdk.zip > flotblancs/javafx-sdk.zip
 
 echo "Unzipping"
-tar -xf javafx-sdk.zip
-mv javafx-sdk-* javafx-sdk
+tar -xf flotblancs/javafx-sdk.zip
+mv flotblancs/javafx-sdk-* flotblancs/javafx-sdk
 
 mvn clean package -DskipTests
 
-mkdir flotblancs
 mv target/flots-blancs-1.0.0.jar flotblancs
