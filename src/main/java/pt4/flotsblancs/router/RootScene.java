bd39a7119c4fd58342e5163d277953b59eaaee93
Gallon Emilien <emilien.gallon@etu.u-bordeaux.fr>
2022-03-24 08:31:58 +0100

    Fix build !

--- src/main/java/pt4/flotsblancs/router/RootScene.java
+++ src/main/java/pt4/flotsblancs/router/RootScene.java
@@ -1,5 +1,7 @@
 package pt4.flotsblancs.router;
 
+import java.io.InputStream;
+
 import javafx.animation.FadeTransition;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
@@ -81,12 +83,13 @@ public class RootScene extends StackPane implements BreakPointListener {
      * Méthode pour charger les resources CSS.
      */
     private void loadStyleSheets() {
-        String baseUrl = "file:src/main/java/pt4/flotsblancs/scenes/stylesheets/";
-        getStylesheets().add(baseUrl + "mfx.css");
-        getStylesheets().add(baseUrl + "index.css");
-        getStylesheets().add(baseUrl + "navBar.css");
-        getStylesheets().add(baseUrl + "windowBar.css");
-        getStylesheets().add(baseUrl + "loginPage.css");
+        String baseUrl = "/css/";
+        
+        getStylesheets().add(baseUrl+"mfx.css");
+        getStylesheets().add(baseUrl+"index.css");
+        getStylesheets().add(baseUrl+"navBar.css");
+        getStylesheets().add(baseUrl+"windowBar.css");
+        getStylesheets().add(baseUrl+"loginPage.css");
     }
 
     /**
