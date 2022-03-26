5ad449ebe1bb660f736957a4f2a480a58405f96d
celian <celian.riboulet@gmail.com>
2022-03-26 17:50:10 +0100

    Begin redesign

--- src/main/java/pt4/flotsblancs/scenes/DashboardScene.java
+++ src/main/java/pt4/flotsblancs/scenes/DashboardScene.java
@@ -1,69 +1,83 @@
 package pt4.flotsblancs.scenes;
 
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import pt4.flotsblancs.Stats;
 import pt4.flotsblancs.Stats.Period;
 import pt4.flotsblancs.router.IScene;
+import pt4.flotsblancs.scenes.components.FlotsBlancsLogo;
 import pt4.flotsblancs.scenes.components.InformationCard;
 import pt4.flotsblancs.scenes.components.VBoxSpacer;
 
-public class DashboardScene extends BorderPane implements IScene {
+public class DashboardScene extends VBox implements IScene {
 
-    @Override
-    public String getName() {
-        return "Accueil";
-    }
+	@Override
+	public String getName() {
+		return "Accueil";
+	}
 
-    @Override
-    public boolean showNavBar() {
-        return true;
-    }
+	/////////////////////////////////////
+	// Nb de client qui quittent
+	// Nb de client qui arrivent
+	// affluence
+	// Temps moyen de réolution de problèmes
+	// emplacments les plus problématiques
+	// emplacements les plus réservés
+	/////////////////////////////////////
 
-    @Override
-    public void onFocus() {
-        
-    }
+	@Override
+	public void start() {
+		setSpacing(40);
 
-    @Override
-    public void onUnfocus() {
-        
-    }
+		var logo = new FlotsBlancsLogo(true, false, 130);
+		
+		this.getChildren().add(new VBoxSpacer());
+		this.getChildren().add(logo);
+		this.getChildren().add(new VBoxSpacer());
+		this.getChildren().add(createStatsContainer());
+		this.getChildren().add(new VBoxSpacer());
 
-    @Override
-    public void start() 
-    {	
-    	try {
-	    	BorderPane stats = new BorderPane();
-	    	
-	    	VBox left = new VBox();
-	    	VBox right = new VBox();
-	    	
-	    	InformationCard c1 = new InformationCard("Résolution des problèmes","Temps moyen, ce mois-ci",Stats.averageProblemTime(Period.MONTHLY)+" jours",Color.rgb(236, 204, 104,0.5f));
-	    	InformationCard c2 = new InformationCard("Occupation","Ce mois-ci",Stats.affluence(Period.MONTHLY)+" emplacements",Color.rgb(164, 176, 190,0.5f));
-	    	InformationCard c3 = new InformationCard("TITRE","Sous-titre","information",Color.rgb(255, 155, 155));
-	    	
-	    	left.getChildren().addAll(c1,c2);
-	    	right.getChildren().add(c3);
-	    	
-	    	/*left.setBackground(new Background(new BackgroundFill(Color.rgb(255, 0, 0), CornerRadii.EMPTY, Insets.EMPTY)));
-	    	left.setPrefWidth(300D);
-	    	left.setPrefHeight(100D);
-	    	
-	    	right.setBackground(new Background(new BackgroundFill(Color.rgb(255, 255, 0), CornerRadii.EMPTY, Insets.EMPTY)));
-	    	right.setPrefWidth(300D);
-	    	right.setPrefHeight(100D);*/
-	    	
-	
-	    	stats.setLeft(left);
-	    	stats.setRight(right);
-	    	
-	        this.setTop(null);
-	        this.setBottom(stats);
-    	} catch(Exception e) {
-    		e.printStackTrace();
-    	}
-    }
+		// try {
+		// BorderPane stats = new BorderPane();
+
+		// VBox left = new VBox();
+		// VBox right = new VBox();
+
+		// InformationCard c1 = new InformationCard("Résolution des problèmes","Temps
+		// moyen, ce mois-ci",Stats.averageProblemTime(Period.MONTHLY)+"
+		// jours",Color.rgb(236, 204, 104,0.5f));
+		// InformationCard c2 = new InformationCard("Occupation","Ce
+		// mois-ci",Stats.affluence(Period.MONTHLY)+" emplacements",Color.rgb(164, 176,
+		// 190,0.5f));
+		// InformationCard c3 = new
+		// InformationCard("TITRE","Sous-titre","information",Color.rgb(255, 155, 155));
+
+		// left.getChildren().addAll(c1,c2);
+		// right.getChildren().add(c3);
+
+		// /*left.setBackground(new Background(new BackgroundFill(Color.rgb(255, 0, 0),
+		// CornerRadii.EMPTY, Insets.EMPTY)));
+		// left.setPrefWidth(300D);
+		// left.setPrefHeight(100D);
+
+		// right.setBackground(new Background(new BackgroundFill(Color.rgb(255, 255, 0),
+		// CornerRadii.EMPTY, Insets.EMPTY)));
+		// right.setPrefWidth(300D);
+		// right.setPrefHeight(100D);*/
+
+		// stats.setLeft(left);
+		// stats.setRight(right);
+
+		// this.setTop(null);
+		// this.setBottom(stats);
+		// } catch(Exception e) {
+		// e.printStackTrace();
+		// }
+	}
+
+	private BorderPane createStatsContainer() {
+		var container = new BorderPane();
+		return container;
+	}
 }
--- src/main/java/pt4/flotsblancs/scenes/components/InformationCard.java
+++ src/main/java/pt4/flotsblancs/scenes/components/InformationCard.java
@@ -13,35 +13,35 @@ import javafx.scene.text.Font;
 
 public class InformationCard extends BorderPane {
 
+  public InformationCard(String mainTitle, String subtitle, String info, Color color) {
 
-    public InformationCard(String mainTitle,String subtitle,String info,Color color) 
-    {
+    this.setPadding(new Insets(25, 25, 25, 25));
+    this.setBackground(new Background(new BackgroundFill(color, new CornerRadii(25), null)));
+    this.setPrefSize(500D, 90D);
 
-       this.setPadding(new Insets(25,25,25,25));
-       this.setBackground(new Background(new BackgroundFill(color, CornerRadii.EMPTY, null)));
-       this.setPrefSize(500D, 90D);
-       
-       Label mainTitleLabel = new Label(mainTitle);
-       Label subtitleLabel = new Label(subtitle);
-       Label infoLabel = new Label(info);
-       
-     /*  mainTitleLabel.setPadding(new Insets(0,0,0,10));
-       subtitleLabel.setPadding(new Insets(0,0,0,10));
-       infoLabel.setPadding(new Insets(0,0,0,-10));*/
-       
-       mainTitleLabel.setFont(new Font(15));
-       subtitleLabel.setFont(new Font(12));
-       infoLabel.setFont(new Font(15));
-       
-       VBox titleBox = new VBox();
-       titleBox.setAlignment(Pos.CENTER);
-       titleBox.getChildren().addAll(mainTitleLabel,subtitleLabel);
-       
-       VBox infoBox = new VBox();
-       infoBox.setAlignment(Pos.CENTER);
-       infoBox.getChildren().addAll(infoLabel);
-       
-       this.setLeft(titleBox);
-       this.setRight(infoBox);
-    }
+    Label mainTitleLabel = new Label(mainTitle);
+    Label subtitleLabel = new Label(subtitle);
+    Label infoLabel = new Label(info);
+
+    /*
+     * mainTitleLabel.setPadding(new Insets(0,0,0,10));
+     * subtitleLabel.setPadding(new Insets(0,0,0,10));
+     * infoLabel.setPadding(new Insets(0,0,0,-10));
+     */
+
+    mainTitleLabel.setFont(new Font(15));
+    subtitleLabel.setFont(new Font(12));
+    infoLabel.setFont(new Font(15));
+
+    VBox titleBox = new VBox();
+    titleBox.setAlignment(Pos.CENTER);
+    titleBox.getChildren().addAll(mainTitleLabel, subtitleLabel);
+
+    VBox infoBox = new VBox();
+    infoBox.setAlignment(Pos.CENTER);
+    infoBox.getChildren().addAll(infoLabel);
+
+    this.setLeft(titleBox);
+    this.setRight(infoBox);
+  }
 }
